0d6da93e37a22e24a1a716d5428e72d515eeed5c
fmoayeri2 <fmoayeri2@myseneca.ca>
2023-09-11 13:19:42 -0400

    a1: Progress

--- a1/a1.js
+++ a1/a1.js
@@ -1,5 +1,6 @@
 const readline = require('readline');
 const fs = require('fs');
+const path = require('path');
 
 const rl = readline.createInterface(process.stdin, process.stdout);
 
@@ -9,13 +10,42 @@ rl.question("Do you wish to process a File (f) or directory (d):  ", function(ch
             if(err) {
                 console.log(err.message);
             } else {
-                let fileContentString = fileContents.toString(); //.replace(/\s+/g, ' ');
-                console.log(fileContents);
+                let fileContentsString = fileContents.toString().replace(/\s+/g, ' ');
+                let fileContentsArray = fileContentsString.replace(/[^\w\s\']/g, "").split(" ");
+                console.log(`Number of Characters (including spaces): ${fileContentsString.length}`);
+                console.log(`Number of Words: ${fileContentsArray.length}`);
+                let i;
+                let savedIndex = 0;
+                for(i = 0; i < fileContentsArray.length; i++) {
+                    if(fileContentsArray[savedIndex].length < fileContentsArray[i].length)
+                    {
+                        savedIndex = i;
+                    }
+                }
+                console.log(`Longest Word: ${fileContentsArray[savedIndex]}`);
             }
         })
         rl.close();
     } else if (choice.toLocaleLowerCase() == "d") {
-
+        fs.readdir('./files', (err, fileContents) => {
+            if(err) {
+                console.log(err.message);
+            } else {
+                console.log(`Files (reverse alphabetical order): ${fileContents.reverse()}`);
+                for(let i = 0; i < fileContents.length; i++) {
+                    fs.readFile(path.join(__dirname, 'files' ,fileContents[i]), (err, textFile) => {
+                        if(err)
+                        {
+                            console.log(err.message)
+                        } else {
+                            console.log(`${fileContents[i]}: ${Buffer.byteLength(textFile, 'uft8')}`);
+                        }
+                    });
+                }
+            }
+        })
+        if('./files')
+        console.log("end");
     } else {
         console.log("Invalid Selection.");
     }
